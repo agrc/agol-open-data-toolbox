352ab4c33b48cb6472c1961e1178877094e0b12e
Jacob Adams <jdadams@utah.gov>
2019-12-06 13:48:41 -0700

    csv logging, exception handling

--- agol-publish/NightStocker.py
+++ agol-publish/NightStocker.py
@@ -9,8 +9,8 @@ import tempfile
 import shutil
 import json
 import pprint
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
 
 def project_data(sgid_table, fgdb_folder, fgdb, is_table):
@@ -163,25 +163,29 @@ def create_service_definition(layer_info, sde_path, temp_dir, project_path,
         end = datetime.datetime.now()
         print('time: {}'.format(end-start))
 
+    except arcpy.ExecuteError:
+        raise  #: pass error up so that it can be logged and continued
+
     finally:
 
-        layer.updateConnectionProperties(os.path.join(temp_dir, 'tempfgdb.gdb'), r'c:\foo\bar.gdb', auto_update_joins_and_relates=False, validate=False)
+        if layer and not is_table:
+            layer.updateConnectionProperties(os.path.join(temp_dir, 'tempfgdb.gdb'), r'c:\foo\bar.gdb', auto_update_joins_and_relates=False, validate=False)
 
-        print(layer.isBroken)
-        agol_map.removeLayer(layer)
-        proj.save()
+            print(layer.isBroken)
+            agol_map.removeLayer(layer)
+            proj.save()
 
-        layer = None
-        cim = None
-        agol_map = None
-        proj = None
-        sharing_draft = None
+        # layer = None
+        # cim = None
+        # agol_map = None
+        # proj = None
+        # sharing_draft = None
 
-        del layer
-        del cim
-        del agol_map
-        del proj
-        del sharing_draft
+        # del layer
+        # del cim
+        # del agol_map
+        # del proj
+        # del sharing_draft
 
         #: Delete feature class
         # print(f'Deleting {projected_table}...')
@@ -194,21 +198,31 @@ def create_service_definition(layer_info, sde_path, temp_dir, project_path,
     return sd_path
 
 #: TODO: finish this method
-def document_action(action_info):
+def log_action(action_info, method, log_path=None):
     '''
     Documents actions to stewardship doc, local csv, and AGOLItems metatable
+    action_info:    a list of info relevant to a single feature class
+    method:         a list of methods (gdoc, csv, or metatable)
     '''
 
+    if 'gdoc' in method:
+        pass
+        # #: reauthorize gspread for each publish to make sure that the auth doesn't time out
+        # scope = ['https://spreadsheets.google.com/feeds',
+        #         'https://www.googleapis.com/auth/drive']
 
-    #: reauthorize gspread for each publish to make sure that the auth doesn't time out
-    scope = ['https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive']
+        # credentials = ServiceAccountCredentials.from_json_keyfile_name('deq-enviro-key.json', scope)
+        # gc = gspread.authorize(credentials)
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('deq-enviro-key.json', scope)
-    gc = gspread.authorize(credentials)
+        # sheet = gc.open_by_key('1MBTwZg7pqpD9noFNAHU8d76EfXD3hMffmbjAHBtkoyQ').get_worksheet(0)
+        # sheet.append_row([item_name, published_id, f'https://utah.maps.arcgis.com/home/item.html?id={published_id}'])
+    if 'csv' in method:
+        if not log_path:
+            raise IOError('Logfile not specified')
+        with open(log_path, 'a') as logfile:
+            log_writer = csv.writer(logfile)
+            log_writer.writerow(action_info)
 
-    sheet = gc.open_by_key('1MBTwZg7pqpD9noFNAHU8d76EfXD3hMffmbjAHBtkoyQ').get_worksheet(0)
-    sheet.append_row([item_name, published_id, f'https://utah.maps.arcgis.com/home/item.html?id={published_id}'])
 
 
 #: TODO: Generalized tag checker method
@@ -221,6 +235,7 @@ map_name = 'AGOL Upload'
 test_fc_name = r'SGID10.BIOSCIENCE.Habitat_BandtailedPigeon'
 list_csv = r'c:\temp\shelved.csv'
 terms_of_use_path = r'l:\sgid_to_agol\termsOfUse.html'
+log_path = r'c:\temp\shelved_log.csv'
 
 temp_dir = tempfile.TemporaryDirectory(prefix='shelved_')
 # arcpy.env.scratchWorkspace = temp_dir.name
@@ -229,16 +244,16 @@ temp_dir = tempfile.TemporaryDirectory(prefix='shelved_')
 agol_user = sys.argv[1]
 gis = arcgis.gis.GIS('https://www.arcgis.com', agol_user, getpass.getpass(prompt='{}\'s password: '.format(agol_user)))
 
-# layers = []
-# with open(list_csv) as list_file:
-#     reader = csv.reader(list_file)
-#     next(reader)
-#     for row in reader:
-#         if row[3] != 'removed': #: Just don't even add removed items to the list
-#             layers.append(row)
+layers = []
+with open(list_csv) as list_file:
+    reader = csv.reader(list_file)
+    next(reader)
+    for row in reader:
+        if row[3] != 'removed': #: Just don't even add removed items to the list
+            layers.append(row)
 
-# test = layers[:3]
-test = [[test_fc_name, 'Bandtailed Pigeon Habitat', 'DWR', 'shelved']]
+test = layers
+# test = [[test_fc_name, 'Bandtailed Pigeon Habitat', 'DWR', 'shelved']]
 
 #: Get metadata for whole SDE, terms of use
 metadata_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'metadata2.json')
@@ -259,80 +274,87 @@ for entry in test:
         'title':entry[1]
     }
 
-    sd_path = create_service_definition(layer_info, sde_path, 
-                                        temp_dir.name, project_path, 
-                                        map_name)
+    try:
+        sd_path = create_service_definition(layer_info, sde_path, 
+                                            temp_dir.name, project_path, 
+                                            map_name)
 
-    category = entry[0].split('.')[-2].title()
-    credit = entry[2] if entry[2] else 'AGRC'
-    
-    #: Get metadata for this specific featureclass
-    metadata = metadata_lookup[entry[0].split('.')[-1]]
 
-    #: Get tags, ensuring AGRC and SGID are in the list
-    base_tags = ['AGRC', 'SGID']
-    if metadata['tags']:
-        tags = metadata['tags'].split(',')
-        for tag in base_tags:
-            if tag not in tags:
-                tags.append(tag)
-    else:
-        tags = base_tags
+        category = entry[0].split('.')[-2].title()
+        credit = entry[2] if entry[2] else 'AGRC'
+        
+        #: Get metadata for this specific featureclass
+        metadata = metadata_lookup[entry[0].split('.')[-1]]
 
-    description = metadata['description']
+        #: Get tags, ensuring AGRC and SGID are in the list
+        base_tags = ['AGRC', 'SGID']
+        if metadata['tags']:
+            tags = metadata['tags'].split(',')
+            for tag in base_tags:
+                if tag not in tags:
+                    tags.append(tag)
+        else:
+            tags = base_tags
 
-    shelved_disclaimer = '<i><b>NOTE</b>: This dataset is an older dataset that we have removed from the SGID and \'shelved\' in ArcGIS Online. There may be a newer vintage of this dataset in the SGID.</i>'
+        description = metadata['description']
 
-    static_disclaimer = '<i><b>NOTE</b>: This dataset holds \'static\' data that we don\'t expect to change. We have removed it from the SDE database and placed it in ArcGIS Online, but it is still considered part of the SGID and shared on opendata.gis.utah.gov.</i>'
+        shelved_disclaimer = '<i><b>NOTE</b>: This dataset is an older dataset that we have removed from the SGID and \'shelved\' in ArcGIS Online. There may be a newer vintage of this dataset in the SGID.</i>'
 
-    if metadata['licenseInfo']:
-        terms = metadata['licenseInfo']
-    else:
-        terms = generic_terms_of_use
+        static_disclaimer = '<i><b>NOTE</b>: This dataset holds \'static\' data that we don\'t expect to change. We have removed it from the SDE database and placed it in ArcGIS Online, but it is still considered part of the SGID and shared on opendata.gis.utah.gov.</i>'
 
-    if entry[3] == 'shelved':
-        group = 'AGRC Shelf'
-        tags.append('shelved')
-        folder = 'AGRC_Shelved'
-        description = f'{shelved_disclaimer} <p> </p> <p>{description}</p>'
-    elif entry[3] == 'static':
-        group = f'Utah SGID {category}'
-        tags.append('static')
-        folder = f'Utah SGID {category}'
-        description = f'{static_disclaimer} <p> </p> <p>{description}</p>'
-    else:
-        raise RuntimeError(f'Unknown shelving category: {entry[3]}')
+        if metadata['licenseInfo']:
+            terms = metadata['licenseInfo']
+        else:
+            terms = generic_terms_of_use
 
-    item_info = {
-        'name': entry[1],
-        'summary': metadata['snippet'][:2047],  #: truncate long snippets
-        'groups': [group],
-        'tags': ', '.join(tags),
-        'description': description,
-        'terms_of_use': terms,
-        'credits': credit,
-        'folder': folder
-    }
+        if entry[3] == 'shelved':
+            group = 'AGRC Shelf'
+            tags.append('shelved')
+            folder = 'AGRC_Shelved'
+            description = f'{shelved_disclaimer} <p> </p> <p>{description}</p>'
+        elif entry[3] == 'static':
+            group = f'Utah SGID {category}'
+            tags.append('static')
+            folder = f'Utah SGID {category}'
+            description = f'{static_disclaimer} <p> </p> <p>{description}</p>'
+        else:
+            raise ValueError(f'Unknown shelving category: {entry[3]}')
 
-    pprint.pprint(item_info)
+        item_info = {
+            'name': entry[1],
+            'summary': metadata['snippet'][:2047],  #: truncate long snippets
+            'groups': [group],
+            'tags': ', '.join(tags),
+            'description': description,
+            'terms_of_use': terms,
+            'credits': credit,
+            'folder': folder
+        }
 
-    item_id = upload_layer(gis, sd_path, item_info, protect=False)
+        # pprint.pprint(item_info)
 
-    describe = arcpy.da.Describe(os.path.join(sde_path, test[0]))
-    shape = describe['shapeType'].lower()
-    dash_name = entry[1].replace(' ', '-').lower
-    endpoint = f'https://opendata.gis.utah.gov/datasets/{dash_name}'
-    data_layer = entry[0].partition('.')[2]  #: layername for stewardship doc
+        # item_id = upload_layer(gis, sd_path, item_info, protect=False)
+        item_id = 'testing'
 
-    #: Log: AGOL title, SGID name for stewardship doc, description, source/credit, shape type, endpoint, AGOL item ID, operation
-    log_entry = [entry[1], data_layer, description, credit, shape, endpoint, item_id, entry[3]]
-    log.append(log_entry)
+        describe = arcpy.da.Describe(os.path.join(sde_path, entry[0]))
+        shape = describe['shapeType'].lower()
+        dash_name = entry[1].replace(' ', '-').lower
+        endpoint = f'https://opendata.gis.utah.gov/datasets/{dash_name}'
+        data_layer = entry[0].partition('.')[2]  #: layername for stewardship doc
 
-    #: Delete files from the scratch folder
-    # sddraft = sd_path + 'draft'
-    # os.remove(sd_path)
-    # os.remove(sddraft)
+        #: Log: AGOL title, operation, SGID name for stewardship doc, description, source/credit, shape type, endpoint, AGOL item ID
+        log_entry = [entry[1], entry[3], data_layer, description, credit, shape, endpoint, item_id]
+        log.append(log_entry)
+        log_action(log_entry, ['csv'], log_path)
 
+        #: Delete files from the scratch folder
+        # sddraft = sd_path + 'draft'
+        # os.remove(sd_path)
+        # os.remove(sddraft)
+    except arcpy.ExecuteError:
+        message = arcpy.GetMessages()
+        print(message)
+        log_action([entry[1], message.replace(',', ';')], 'csv', log_path)
     #: TODO create log of updates, either directly to stewardship or as csv
 
     #: TODO add AGOL link to stewardship doc after item is shelved/staticed?
