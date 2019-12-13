566bb18da871937c68c27789f5958b02205bb1bb
Jake Adams <jdadams@utah.gov>
2019-12-13 12:23:57 -0700

    testing note

--- agol-publish/settings.py
+++ agol-publish/settings.py
@@ -5,5 +5,6 @@ LIST_CSV = r'c:\temp\shelved.csv'
 TERMS_OF_USE_PATH = r'l:\sgid_to_agol\termsOfUse.html'
 LOG_PATH = r'c:\temp\shelved_log.csv'
 GSHEET_AUTH = r'c:\gis\git\agol-open-data-toolbox\agol-publish\client_secret.json'
+#: Note: these currently point to testing sheets.
 STEWARDSHIP_SHEET_KEY = '1Qu60mevJHwCvBAWAk6bF2NhwEykh5znWInaGzsxWG1c'
 AGOL_SHEET_KEY = '1jPJcu3zLYvbaaksFEr_ySa1dreASEoGU15n3KcLdp0w'
